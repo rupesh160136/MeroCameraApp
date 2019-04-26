e411fa8299c96462c08b792d7b65b7fe062bc22e
Softwarica College Lab <softwarica@softwarica.com>
2019-04-26 08:09:56 +0545

    camera is loaded in activity

--- app/src/main/java/np/com/softwarica/merocameraapp/MainActivity.java
+++ app/src/main/java/np/com/softwarica/merocameraapp/MainActivity.java
@@ -1,20 +1,40 @@
 package np.com.softwarica.merocameraapp;
 
 import android.Manifest;
+import android.app.DownloadManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCamera = findViewById(R.id.button_image);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadCamera();
+
+            }
+        });
         checkPermission();
+    }
+    private void loadCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(intent, 1);
+        }
     }
     private void checkPermission(){
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
