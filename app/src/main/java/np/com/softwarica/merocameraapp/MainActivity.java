bf23f51f39c27cada9e6747775be7e0f31e57002
Softwarica College Lab <softwarica@softwarica.com>
2019-04-26 07:56:11 +0545

    Initial commit

---
+++ app/src/main/java/np/com/softwarica/merocameraapp/MainActivity.java
@@ -0,0 +1,25 @@
+package np.com.softwarica.merocameraapp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkPermission();
+    }
+    private void checkPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
+    }
+
+}
